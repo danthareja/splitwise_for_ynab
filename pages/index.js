2d26faddc5c6822ac1072b655b647c2580c1d052
Dan Thareja <danthareja@gmail.com>
2024-11-06 12:51:53 -0800

    Send LA ynab txns to splitwise

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,21 @@ export default function Home() {
       )
       .then((res) => res.data.data.transactions);
 
-    const transactions = [...myTransactions, ...partnerTransactions];
+    const laTransactions = await axios
+      .post(
+        "/api/ynab_to_splitwise",
+        {
+          who: "2024.11 LA",
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${inputRef.current.value}`,
+          },
+        }
+      )
+      .then((res) => res.data.data.transactions);
+
+    const transactions = [...myTransactions, ...partnerTransactions, ...laTransactions];
 
     const myExpenses = await axios
       .post(
